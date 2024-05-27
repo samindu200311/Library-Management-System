7b7f900886169ddb339d98ebbbb92ff778751cce
samindu200311 <ssamindudeneth04@gmail.com>
2024-05-27 11:17:15 +0530

    Library Management System

---
+++ src/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    public static void main(String[] args) {
+        System.out.println("Book Name");
+        System.out.println("Book ID");
+        System.out.println("Author");
+    }
+
+}
---
+++ src/Library_System.java
@@ -0,0 +1,7 @@
+public class Library_System {
+    public static void main(String[] args) {
+        System.out.println("Librarian Name");
+        System.out.println("Assistant Librarian");
+        System.out.println("Date");
+    }
+}
